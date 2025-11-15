62df75b568ce357706efdb46068946bbd8bce8bf
RSU SafeSpace <rsusafespace@gmail.com>
2025-11-16 01:14:54 +0800

    Update firebase_config.js

--- firebase_config.js
+++ firebase_config.js
@@ -4,15 +4,17 @@
 (function() {
   // Firebase config for rsu-safespace (used by verification pages)
   // NOTE: keep this file out of public repos if these keys are sensitive for your workflow.
+  require('dotenv').config();
+  
   var firebaseConfig = {
-    apiKey: "AIzaSyD4eMHzsieWnIH6nHLgBl1PDTiIETeVmnA",
-    authDomain: "rsu-safespace.firebaseapp.com",
-    databaseURL: "https://rsu-safespace-default-rtdb.firebaseio.com",
-    projectId: "rsu-safespace",
-    storageBucket: "rsu-safespace.firebasestorage.app",
-    messagingSenderId: "490237933031",
-    appId: "1:490237933031:web:0d17829f4359da952db942",
-    measurementId: "G-YY33W1QM2N"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
   };
 
   try {
@@ -234,4 +236,5 @@
   }
 
 })();
+
 
